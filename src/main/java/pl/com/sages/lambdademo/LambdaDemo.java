189793805b54dbbd5cb1d678420ab7429d45ad27
Marcin Chrost <marcin.chrost@jcommerce.pl>
2019-09-17 10:27:07 +0200

    lambda examples

--- src/main/java/pl/com/sages/lambdademo/LambdaDemo.java
+++ src/main/java/pl/com/sages/lambdademo/LambdaDemo.java
@@ -4,16 +4,39 @@ import java.util.Optional;
 import java.util.function.Function;
 
 public class LambdaDemo {
+
+    static private Function<Book, String> titleExtractor = new TitleExtractor();
+
+    static private Function<Book, String> titleExtractor2 = new Function<Book, String>() {
+        @Override
+        public String apply(Book book) {
+            return book.getTitle();
+        }
+    };
+
+    static private Function<Book, String> titleExtractor3 = book -> {
+        return book.getTitle();
+    };
+
+    static private Function<Book, String> titleExtractor4 = book -> book.getTitle();
+
+    static private Function<Book, String> titleExtractor5 = Book::getTitle;
+
     public static void main(String[] args) {
         //System.out.println("Hello World");
 
         var book = new Book();
         book.setTitle("Dziady");
-        System.out.println(extractTitleFromOptional(Optional.ofNullable(book)));
+        System.out.println(extractTitle(book));
 
         Book noBook = null;
-        System.out.println(extractTitleFromOptional(Optional.empty()));
+        System.out.println(extractTitle(noBook));
         //var book = (Book)null;
+
+        var book2 = Book.builder().title("Wladca pierscieni").build();
+        var title2 = titleExtractor.apply(book2);
+
+        System.out.println(title2);
 
 
     }
@@ -28,6 +51,26 @@ public class LambdaDemo {
     }
 
     public static String extractTitleFromOptional(Optional<Book> optionalBook) {
-        return optionalBook.map(Book::getTitle).orElse("Brak tytułu");
+        Optional<String> title = optionalBook.map(new Function<Book, String>() {
+            @Override
+            public String apply(Book book) {
+                return book.getTitle();
+            }
+        });
+        String extractedTitle = title.orElse("Brak tytułu");
+        return extractedTitle;
+
+
+    }
+
+
+
+}
+
+class TitleExtractor implements Function<Book, String> {
+
+    @Override
+    public String apply(Book book) {
+        return book.getTitle();
     }
 }
